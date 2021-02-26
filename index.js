cefb3502334a2dfcaf049cf2ad9e54a1beb3daed
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-02-26 17:59:59 +0000

    feat: ✨Add rules module

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  globals: {
+    MyGlobal: true
+  },
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    indent: ["error", "tab"],
+  }
+
+};
